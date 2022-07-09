b52f25a16542dcf95d26ca4ea532f5672a27400c
Vivek Mishra <100134407+NatwarBHAI@users.noreply.github.com>
2022-07-09 10:41:32 +0530

    basic movie website

---
+++ scripts/stream.js
@@ -0,0 +1,154 @@
+let MovieArr = JSON.parse(localStorage.getItem("Movies"));
+streaming(MovieArr);
+function streaming() {
+    let Name = document.querySelector("#Moviename");
+    MovieArr.forEach(function (element) {
+        if (element == "Pushpa") {
+            Name.innerText = element;
+            let x = document.createElement("source");
+            x.src = "https://tiny-glitter-5fee.dadeya1837.workers.dev/C83BWdIM3C0iXJq7H61XG2PG0ZlVGhziNI9Gk9Mq/gd2.botworker.xyz/Pushpa%20720p%20VivekMishra.com.mkv";
+            document.querySelector("#video").append(x);
+            localStorage.clear();
+        } else if (element == "RRR") {
+            Name.innerText = element;
+            let x = document.createElement("source");
+            x.src = "https://frosty-cherry-a058.jevebo2608.workers.dev/b00nFF7TjLyhjPwHIj7hOMsY2RDBPlGalAKjfGVZ/gd2.botworker.xyz/RRR%20(2022)%20Hindi%20Dubbed%20720p%20NF%20WEBDL%20ESub%20VivekMishra.com.mkv";
+            document.querySelector("#video").append(x);
+            localStorage.clear();
+        } else if (element == "KGF Chapter 2") {
+            Name.innerText = element;
+            let x = document.createElement("source");
+            x.src = "https://cool-heart-1518.mawila2578.workers.dev/PqKvrzrfF0rx5muwLTIDb9dqpNFFBPucqKnLuiCn/gd2.botworker.xyz/K.G.F.%20Chapter%202%20(2022)%20ORG%20Hindi%20Dubbed%20720p%20WEBDL%20ESub%20VivekMishra.com.mkv";
+            document.querySelector("#video").append(x);
+            localStorage.clear();
+        } else if (element == "Pati Patni or Woh") {
+            Name.innerText = element;
+            let x = document.createElement("source");
+            x.src = "https://nameless-mud-3fdc.vecefe1221.workers.dev/vjj9ngFge4tU0bFHIKeQCQIabbid2kcPnXJPFFK4/gd2.botworker.xyz/Pati.Patni.Aur.Woh.2019.720p.VivekMishra.com.mkv";
+            document.querySelector("#video").append(x);
+            localStorage.clear();
+        } else if (element == "Jugjugg Jiyo") {
+            Name.innerText = element;
+            let x = document.createElement("source");
+            x.src = "https://young-feather-7fbd.vodom62332.workers.dev/nU9EQMxCN7oI5ou2uh9Z6mAQUkNlwPj3cjE6qQxb/gd2.botworker.xyz/Jug.Jugg.Jeeyo.2022.720p.VivekMishra.com.mkv";
+            document.querySelector("#video").append(x);
+            localStorage.clear();
+        } else if (element == "Anek") {
+            Name.innerText = element;
+            let x = document.createElement("source");
+            x.src = "https://quiet-shape-800d.lemotoj255.workers.dev/ltreEM6mJgrpzNGfPjid4CnLJxjyYQML5m4yKe3q/gd2.botworker.xyz/Anek%20(2022)%20Hindi%20720p%20WEBDL%20ESub%20VivekMishra.com.mkv";
+            document.querySelector("#video").append(x);
+            localStorage.clear();
+        } else if (element == "Attack") {
+            Name.innerText = element;
+            let x = document.createElement("source");
+            x.src = "https://sparkling-snowflake-4938.tarak82340.workers.dev/IaTQxHWUNCPObxfWWwWV1y3bH8A8UVsZCLGFAlG4/gd2.botworker.xyz/Attack%20%20Part%201%20(2022)%20Hindi%20720p%20WEBDL%20ESub%20VivekMishra.com.mkv";
+            document.querySelector("#video").append(x);
+            localStorage.clear();
+        } else if (element == "Jayeshbhai Jordaar") {
+            Name.innerText = element;
+            let x = document.createElement("source");
+            x.src = "https://polished-sound-d4a5.roveti2608.workers.dev/qXk6qXi5sMBhkhcREdh496sfpqXXxqLGnpJdtYcb/gd2.botworker.xyz/Jayeshbhai%20Jordaar%20(2022)%20Hindi%20720p%20WEBDL%20ESub%20VivekMishra.com.mkv";
+            document.querySelector("#video").append(x);
+            localStorage.clear();
+        } else if (element == "Veere Di Wedding") {
+            Name.innerText = element;
+            let x = document.createElement("source");
+            x.src = "https://nameless-mud-3fdc.vecefe1221.workers.dev/xWWBzDDd2dR08hc7YXarhohxnLXAFDkU5nI4YolZ/gd2.botworker.xyz/Veere.Di.Wedding.1080p.themoviesflix.in.mkv";
+            document.querySelector("#video").append(x);
+            localStorage.clear();
+        } else if (element == "Hello Charlie") {
+            Name.innerText = element;
+            let x = document.createElement("source");
+            x.src = "https://frosty-breeze-a6a2.vicit51535.workers.dev/Nuc8TQIFhhwqheBuV4tXN603bU3LbAsGbjT6NdOD/gd2.botworker.xyz/Hello.Charlie.2021.1080p.Hindi.AMZN.WEBDL.DD5.1.x264Altmovies.in.mkv";
+            document.querySelector("#video").append(x);
+            localStorage.clear();
+        } else if (element == "Dhamaal") {
+            Name.innerText = element;
+            let x = document.createElement("source");
+            x.src = "https://twilight-salad-b442.wicawab874.workers.dev/ZMGeyH636TDco7LEBm78GbpruNCzuQvAArCDP28P/gd2.botworker.xyz/Dhamaal.720p.VivekMishra.com.mkv";
+            document.querySelector("#video").append(x);
+            localStorage.clear();
+        } else if (element == "Chennai Express") {
+            Name.innerText = element;
+            let x = document.createElement("source");
+            x.src = "https://mute-haze-e132.cegiw33856.workers.dev/yM3lBwygcJjHi9oLZBusRUSyKgKIt2PEJ4u54e6a/gd2.botworker.xyz/Chennai.Express.2013.720p.BluRay.VivekMishra.com.mkv";
+            document.querySelector("#video").append(x);
+            localStorage.clear();
+        } else if (element == "Coolie No. 1") {
+            Name.innerText = element;
+            let x = document.createElement("source");
+            x.src = "https://dry-snow-80d5.sepagi8306.workers.dev/q3kaVCd8NjHRbJM1H0DtAKbUssVwDiaFFC2iVLEP/gd2.botworker.xyz/Coolie.No.1.2020.Hindi.720p.WEBDL.DD5.1.ESub.x264VivekMishra.com.mkv";
+            document.querySelector("#video").append(x);
+            localStorage.clear();
+        } else if (element == "Dabangg") {
+            Name.innerText = element;
+            let x = document.createElement("source");
+            x.src = "https://snowy-wind-b222.cologeb952.workers.dev/BpmvvvgqqXQ8ZNuAOnOtmtt6FpX0G9WOfgUQkNlF/gd2.botworker.xyz/Dabangg.720p.VivekMishra.com.mkv";
+            document.querySelector("#video").append(x);
+            localStorage.clear();
+        } else if (element == "KGF Chapter 1") {
+            Name.innerText = element;
+            let x = document.createElement("source");
+            x.src = "https://yellow-limit-dd5c.keleh93440.workers.dev/KUyCxe2aCCnnuB5qOS84n5HftEnRplyR1l3vf4j6/gd2.botworker.xyz/K.G.F..Chapter.1.(2018).Hindi.Dubbed.720p.WEBDL.ESub.VivekMishra.com.mkv";
+            document.querySelector("#video").append(x);
+            localStorage.clear();
+        } else if (element == "Deadpool 2") {
+            Name.innerText = element;
+            let x = document.createElement("source");
+            x.src = "https://production.polished-forest-savaan.ffib9we.workers.dev/6XMouaSRR12BDBt0YNvu4UTYCPYyNyyVf4feyRAz/gd4.botworker.xyz/Deadpool.2.2018.UNRATED.720p.Bluray.Hindi.English.MoviesVerse.App.mkv";
+            document.querySelector("#video").append(x);
+            localStorage.clear();
+        }
+        else if (element == "Mugguru Monagallu") {
+            Name.innerText = element;
+            let x = document.createElement("source");
+            x.src = "https://morning-river-fca4.cevodi5919.workers.dev/d2y7JzTLHghbgGabIOlA37UPibSpd1pd6bKIud76/gd2.botworker.xyz/Mugguru.Monagallu.2021.720p.WEBDL.Hindi.ORG.5.1Telugu.ESub.x264Dogemovies.xyz.mkv";
+            document.querySelector("#video").append(x);
+            localStorage.clear();
+        } else if (element == "One Two Three") {
+            Name.innerText = element;
+            let x = document.createElement("source");
+            x.src = "https://withered-pond-2647.koxisog626.workers.dev/wYS31GkpDdvUvTtUUfpLTMnwlyJFm8BZkOpZoFqG/gd2.botworker.xyz/One.Two.Three.2008.Hindi.720p.WEBDLthemoviesflix.in.mkv";
+            document.querySelector("#video").append(x);
+            localStorage.clear();
+        } else if (element == "Zindagi Na Milegi Dobara") {
+            Name.innerText = element;
+            let x = document.createElement("source");
+            x.src = "https://withered-pond-2647.koxisog626.workers.dev/timJBNEpP6J7wwvK85YdVnw5i2VOuUmNYfInLPwV/gd2.botworker.xyz/Zindagi.Na.Milegi.Dobara.2011.720p.HDRip.themoviesflix.in.mp4";
+            document.querySelector("#video").append(x);
+            localStorage.clear();
+        } else if (element == "Haunted") {
+            Name.innerText = element;
+            let x = document.createElement("source");
+            x.src = "https://young-fire-9a25.jowow63438.workers.dev/8pYljO7XeT2r8aOJR4cv4bTQ7nKEZhtPer32yYo1/gd2.botworker.xyz/Haunted.3D.720p.HDRip.themoviesflix.in.mkv";
+            document.querySelector("#video").append(x);
+            localStorage.clear();
+        } else if (element == "Robot") {
+            Name.innerText = element;
+            let x = document.createElement("source");
+            x.src = "https://lingering-bonus-a992.tinovim380.workers.dev/t62Q8WTtDHpymDiUmMme57I8mu1qR3Ja3dFEPVHC/gd2.botworker.xyz/Robot.720p.VivekMishra.com.mkv";
+            document.querySelector("#video").append(x);
+            localStorage.clear();
+        } else if (element == "Welcome") {
+            Name.innerText = element;
+            let x = document.createElement("source");
+            x.src = "https://twilight-meadow-b687.joyis20224.workers.dev/HB6YNBMG9Qml8F0IQp4cGApAYOd2Xq4oemAYvLlE/gd2.botworker.xyz/Welcome.2007.1080p.themoviesflix.in.mkv";
+            document.querySelector("#video").append(x);
+            localStorage.clear();
+        } else if (element == "Annabelle Sethupathi") {
+            Name.innerText = element;
+            let x = document.createElement("source");
+            x.src = "https://dry-truth-dc0b.cofes17767.workers.dev/kgvMkLrgRfogK7XGCAymitwyy24ezHCljj6y92be/gd2.botworker.xyz/Annabelle.Sethupathi.2021.Hindi.720p.WEBDL.DD5.1.ESub.x264VivekMishra.com.mkv";
+            document.querySelector("#video").append(x);
+            localStorage.clear();
+        }
+        else if (element == "Queen") {
+            Name.innerText = element;
+            let x = document.createElement("source");
+            x.src = "https://little-smoke-48ef.rerak47202.workers.dev/VKvy9Y3sgfWk2UkvqM1jeg4H4yljsRyv0juRQXAM/gd2.botworker.xyz/Queen.2013.720p.BluRay.themoviesflix.iin.mkv";
+            document.querySelector("#video").append(x);
+            localStorage.clear();
+        }
+    });
+}
